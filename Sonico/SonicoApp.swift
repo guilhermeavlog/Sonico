5a4ff253373e280f671a45bcb9a8c44fe6476028
guilhermeavlog <guilhermeloges@gmail.com>
2025-12-10 01:48:24 -0700

    Initial Commit

---
+++ Sonico/SonicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicoApp.swift
+//  Sonico
+//
+//  Created by Guilherme Loges on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SonicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
